17a76e915017414f92bc3f11b600f05ce41a0351
abdulfastcode <abdul@fastcode.ai>
2026-01-19 16:06:08 +0530

    Update team page description to focus on the team's commitment to innovative solutions, removing details about individual backgrounds for a more concise presentation.

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
       <Hero
         HeroImage={HeroImage}
         title="Exceptional Team Lead To Exceptional Results"
-        description="We are a group of ambitious AI experts and coders committed to delivering innovative solutions. Our passion for excellence drives us to redefine the boundaries of technology.Our team is composed of seasoned professionals, each with extensive research backgrounds. We are proud authors of influential papers and active contributors to open-source initiatives. Each member has been carefully selected from top universities, emphasizing our commitment to innovative problem solvers who excel in their fields."
+        description="We are a group of ambitious AI experts and coders committed to delivering innovative solutions. Our passion for excellence drives us to redefine the boundaries of technology."
         buttonText="Learn More"
         buttonLink="/team"
       />
