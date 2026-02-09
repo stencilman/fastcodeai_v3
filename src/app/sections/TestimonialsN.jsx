b17e4213dae835d332af7259a8547cd7df773315
abdulfastcode <abdul@fastcode.ai>
2026-02-09 16:09:25 +0530

    Refactor global styles and remove unused CSS files; consolidate theme variables and base styles into globals.css, while commenting out Swiper imports in relevant components to streamline the codebase.

--- src/app/sections/TestimonialsN.jsx
+++ src/app/sections/TestimonialsN.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 import Swiper from "swiper";
 import { Navigation, Autoplay, Keyboard, Controller } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
+// import "swiper/css";
+// import "swiper/css/navigation";
 
 const TestimonialsN = () => {
   useEffect(() => {
--- src/app/sections/TestimonialsSlider.jsx
+++ src/app/sections/TestimonialsSlider.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Controller } from "swiper/modules";
-import "swiper/css";
+// import "swiper/css";
 // import "swiper/css/auto-height"; // Commented out to avoid error if file doesn't exist, will try if needed.
 import Button from "../component/Button";
 
